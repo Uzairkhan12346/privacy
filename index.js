e8a53b710b224fc7cecb718aad81a6715ac52918
Uzairkhan12346 <tempmailuzair@gmail.com>
2025-05-17 18:41:13 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="author" content="Uzair Rajput" />
+    <meta http-equiv="refresh" content="0; url=privacy.html" />
+    <title>Redirecting...</title>
+  </head>
+  <body>
+    <p>Redirecting to privacy.html...</p>
+  </body>
+</html>
